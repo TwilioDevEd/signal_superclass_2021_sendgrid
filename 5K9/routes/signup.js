d87605fcfb0cb2b47d6477df07c5ec430588d1ad
Wade Christensen <wchristensen@twilio.com>
2021-10-18 15:55:11 -0700

    Create app starting point

--- 5K9/routes/signup.js
+++ 5K9/routes/signup.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const sendEmail = require("../controllers/sendEmail");
 
 router.get("/", function (req, res, next) {
   res.render("signup");
 });
 
 router.post("/confirmation", function (req, res, next) {
-  sendEmail(req.body.email, req.body);
+  // TODO
+  // Send confirmation email
   res.render("signup-confirmation");
 });
 
